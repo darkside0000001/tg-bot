2a754f73b8ecf6e43ad23b77db198013c059db09
Kirill.Kleshchev1 <theobject705@gmail.com>
2022-10-08 20:47:48 +0500

    Run tests on build

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,24 +1,23 @@
 import org.example.Bot;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
+import static org.junit.jupiter.api.Assertions.*;
 public class Tests {
     Bot bot = new Bot();
     @Test
     public void getToken() {
-        Assert.assertEquals(System.getenv("BOT_TOKEN"), bot.getBotToken());
+        assertEquals(System.getenv("BOT_TOKEN"), bot.getBotToken());
     }
     @Test
     public void getName() {
-        Assert.assertEquals(System.getenv("BOT_NAME"), bot.getBotUsername());
+        assertEquals(System.getenv("BOT_NAME"), bot.getBotUsername());
     }
     @Test
     public void testHelp() {
-        Assert.assertEquals("Бот может копировать сообщения", bot.parseMessage("/help"));
+        assertEquals("Бот может копировать сообщения", bot.parseMessage("/help"));
     }
     @Test
     public void testCopy() {
-        Assert.assertEquals("Привет мир", bot.parseMessage("Привет мир"));
+        assertEquals("Привет мир", bot.parseMessage("Привет мир"));
     }
 }
