c3f1ae1f5fa25c310216c4f8964e67657f1b4267
Kirill.Kleshchev1 <theobject705@gmail.com>
2022-11-28 17:17:07 +0500

    Исправление тестов.

---
+++ src/main/java/org/example/EventLoopConsole.java
@@ -0,0 +1,39 @@
+package org.example;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+
+public class EventLoopConsole  {
+    Database db = new Database();
+    BotLogic bl = new BotLogic(db);
+    public EventLoopConsole() throws Exception {
+        while (true) {
+            ArrayList<Long> users = db.getAllIntervals();
+            for (Long user : users) {
+                sendDiscounts(user);
+            }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     *Метод, который отправляет товары со скидками
+     */
+    private void sendDiscounts(long chatId) throws Exception {
+        System.out.println(bl.parseMessage("Посмотреть скидки", 0, "cons").get(0));
+}
+    }
---
+++ src/main/java/org/example/EventLoopTelegram.java
@@ -0,0 +1,75 @@
+package org.example;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+
+public class EventLoopTelegram extends TelegramLongPollingBot {
+    Database db = new Database();
+    BotLogic bl = new BotLogic(db);
+    public String BotToken = System.getenv("BOT_TOKEN");
+    public EventLoopTelegram() throws Exception {
+        while (true) {
+            ArrayList<Long> users = db.getAllIntervals();
+            for (Long user : users) {
+                if (user != (long) 0) {
+                    sendDiscounts(user);
+                }
+            }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    /**
+     *Получение имени бота
+     */
+    public String getBotUsername() {
+        return "MBot";
+    }
+
+    @Override
+    /**
+     *Получение токена бота
+     */
+    public String getBotToken() {
+        return BotToken;
+    }
+
+    @Override
+    /**
+     *Метод, который реализует получение и отправку сообщений пользователю
+     */
+    public void onUpdateReceived(Update update) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /**
+     *Метод, который реализует отправку сообщений пользователю
+     */
+    private void sendMessage(Long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *Метод, который отправляет товары со скидками
+     */
+    private void sendDiscounts(long chatId) throws Exception {
+        sendMessage(chatId, (String) bl.parseMessage("Посмотреть скидки", 0, "tele").get(0));
+    }
+}
+
